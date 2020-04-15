9e1b3870c129007eaae233ac2a9150f251f8969a
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-04-15 17:00:05 -0300

    Criando operação de instanciação

--- src/main/java/com/joaogcm/springbackend/SpringBackendApplication.java
+++ src/main/java/com/joaogcm/springbackend/SpringBackendApplication.java
@@ -1,13 +1,31 @@
 package com.joaogcm.springbackend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBackendApplication {
+import com.joaogcm.springbackend.entities.Categoria;
+import com.joaogcm.springbackend.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringBackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Livros");
+		Categoria cat2 = new Categoria(null, "Bebidas");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
