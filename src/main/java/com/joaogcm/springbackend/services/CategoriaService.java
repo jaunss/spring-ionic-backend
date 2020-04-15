33c0fc9b2070c1cb22deff63c1585e4401f40537
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-04-15 16:47:05 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/joaogcm/springbackend/services/CategoriaService.java
+++ src/main/java/com/joaogcm/springbackend/services/CategoriaService.java
@@ -1,5 +1,21 @@
 package com.joaogcm.springbackend.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaogcm.springbackend.entities.Categoria;
+import com.joaogcm.springbackend.repositories.CategoriaRepository;
+
+@Service
 public class CategoriaService {
 	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
 }
