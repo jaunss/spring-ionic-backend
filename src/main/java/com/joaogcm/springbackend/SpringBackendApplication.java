affe636985a7369b32ea7a68772173bf92e4e1c2
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-04-16 10:31:53 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/joaogcm/springbackend/SpringBackendApplication.java
+++ src/main/java/com/joaogcm/springbackend/SpringBackendApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.joaogcm.springbackend.entities.Categoria;
+import com.joaogcm.springbackend.entities.Produto;
 import com.joaogcm.springbackend.repositories.CategoriaRepository;
+import com.joaogcm.springbackend.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringBackendApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApplication.class, args);
@@ -26,6 +31,18 @@ public class SpringBackendApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Livros");
 		Categoria cat2 = new Categoria(null, "Bebidas");
 		
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 }
--- src/main/java/com/joaogcm/springbackend/entities/Produto.java
+++ src/main/java/com/joaogcm/springbackend/entities/Produto.java
@@ -8,12 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria implements Serializable {
+@Table(name = "tb_produto")
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -21,18 +23,22 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_produto_categoria",
+	joinColumns = @JoinColumn(name = "produto_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<Produto>();
-	
-	public Categoria() {
+	public Produto() {
 		
 	}
 	
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 	
 	public Integer getId() {
@@ -51,12 +57,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 	
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 	
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+	
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -75,7 +89,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
