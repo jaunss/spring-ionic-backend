36f47be6ad7c260c964467ba1a6f3b0681c301b6
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-04-24 12:41:25 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/joaogcm/springbackend/services/CategoriaService.java
+++ src/main/java/com/joaogcm/springbackend/services/CategoriaService.java
@@ -21,4 +21,9 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Causa: " + Categoria.class.getName())
 				);
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
