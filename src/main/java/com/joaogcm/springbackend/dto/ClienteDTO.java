c0db381024037b59717f8b77fb4cef8fb7689c95
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-05-08 11:09:11 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/joaogcm/springbackend/dto/ClienteDTO.java
+++ src/main/java/com/joaogcm/springbackend/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.joaogcm.springbackend.entities.Cliente;
+import com.joaogcm.springbackend.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
