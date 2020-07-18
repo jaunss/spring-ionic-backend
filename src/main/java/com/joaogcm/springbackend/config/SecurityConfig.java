580364fcba1274a8bff208ad290395c23a0bbeeb
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-07-18 11:12:43 -0300

    Implementando autorização

--- src/main/java/com/joaogcm/springbackend/config/SecurityConfig.java
+++ src/main/java/com/joaogcm/springbackend/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.joaogcm.springbackend.security.JWTAuthenticationFilter;
+import com.joaogcm.springbackend.security.JWTAuthorizationFilter;
 import com.joaogcm.springbackend.security.JWTUtil;
 
 @Configuration
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
